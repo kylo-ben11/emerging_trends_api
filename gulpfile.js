b888147f96c73e7c874911d92e6ee9230f559b69
kylo-ben11 <ben.knier@gmail.com>
2018-10-11 11:09:39 -0500

    add sass, gulp, and change hamburger size

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+// Requires the gulp-sass plugin
+var sass = require('gulp-sass');
+
+
+
+gulp.task('sass', function(){
+  return gulp.src('source-files')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('destination'))
+});
+
+gulp.task('sass', function(){
+  return gulp.src('css/scss/styles.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(gulp.dest('css/styles.css'))
+});
